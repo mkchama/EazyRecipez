3991dff03f5ddb71f462d0f5d076b243a0fce904
mkchama <mkchama@ucalgary.ca>
2021-04-14 04:15:30 -0600

    Added a rating system to one recipe.
    Will include ratings for all other recipes in next commit

--- EazyRecipez/RecipePage.xaml.cs
+++ EazyRecipez/RecipePage.xaml.cs
@@ -26,7 +26,64 @@ namespace EazyRecipez
     /// </summary>
     public partial class RecipePage : Page
     {
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register("Rating", typeof(int), typeof(RecipePage),
+            new FrameworkPropertyMetadata(0, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, new PropertyChangedCallback(RatingChanged)));
 
+        private int _max = 5;
+
+        public int Value
+        {
+            get
+            {
+                return (int)GetValue(ValueProperty);
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    SetValue(ValueProperty, 0);
+                }
+                else if (value > _max)
+                {
+                    SetValue(ValueProperty, _max);
+                }
+                else
+                {
+                    SetValue(ValueProperty, value);
+                }
+            }
+        }
+
+        private static void RatingChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            RecipePage item = sender as RecipePage;
+            int newval = (int)e.NewValue;
+            UIElementCollection childs = ((Grid)(item.Content)).Children;
+
+            ToggleButton button = null;
+
+            for (int i = 0; i < newval; i++)
+            {
+                button = childs[i] as ToggleButton;
+                if (button != null)
+                    button.IsChecked = true;
+            }
+
+            for (int i = newval; i < childs.Count; i++)
+            {
+                button = childs[i] as ToggleButton;
+                if (button != null)
+                    button.IsChecked = false;
+            }
+
+        }
+
+        private void ClickEventHandler(object sender, RoutedEventArgs args)
+        {
+            ToggleButton button = sender as ToggleButton;
+            int newvalue = int.Parse(button.Tag.ToString());
+            Value = newvalue;
+        }
         public RecipePage()
         {
             InitializeComponent();
